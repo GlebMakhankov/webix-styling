30b71cd034a007f8936f1bbc0e84f9e345e33fa5
= <=>
2021-10-01 00:12:19 +0300

    Code refactoring

--- src/components/myCustomBtn.js
+++ src/components/myCustomBtn.js
@@ -3,24 +3,31 @@ export default webix.protoUI(
     name: "myCustomButton",
     $cssName: "myCustomButton",
     $init: function (config) {
-      function addCssClass(that) {
-        webix.html.addCss(
-          that.getNode(),
-          `myCustomButton_option_${config.state}`
-        );
-      }
-      addCssClass(this);
+      this.addCssClass();
       config.value = config.states[config.state];
       this.attachEvent("onItemClick", function () {
-        config.state !== Object.keys(config.states).length - 1
-          ? config.state++
-          : (config.state = 0);
-        for (let key in config.states) {
-          webix.html.removeCss(this.getNode(), `myCustomButton_option_${key}`);
-        }
-        addCssClass(this);
-        this.callEvent("onStateChange", [config.state, config.states]);
+        this.stateChange();
       });
+    },
+
+    addCssClass() {
+      const config = this.config;
+      webix.html.addCss(
+        this.getNode(),
+        `myCustomButton_option_${config.state || 0}`
+      );
+    },
+
+    stateChange() {
+      const config = this.config;
+      config.state !== Object.keys(config.states).length - 1
+        ? config.state++
+        : (config.state = 0);
+      for (let key in config.states) {
+        webix.html.removeCss(this.getNode(), `myCustomButton_option_${key}`);
+      }
+      this.addCssClass();
+      this.callEvent("onStateChange", [config.state, config.states]);
     },
   },
   webix.ui.button
--- src/components/myCustomForm.js
+++ src/components/myCustomForm.js
@@ -5,45 +5,45 @@ export default webix.protoUI(
       margin: 10,
     },
     $init: function (config) {
-      config.elements = [
-        ...config.fields.map((element) => ({
-          view: "text",
-          label: [element],
-          name: [element],
-        })),
+      config.elements = this.createForm(config);
+    },
+
+    createForm(config) {
+      return [
+        ...this.createFields(config),
         {
           margin: 10,
           cols: [
-            {
-              view: "button",
-              value: "Cancel",
-              click: () =>
-                (
-                  config.cancelAction ??
-                  function () {
-                    webix.message(
-                      `No action was attached to the <strong>Cancel</strong> button`
-                    );
-                  }
-                )(),
-            },
-            {
-              view: "button",
-              value: "Save",
-              css: "webix_primary",
-              click: () =>
-                (
-                  config.saveAction ??
-                  function () {
-                    webix.message(
-                      `No action was attached to the <strong>Save</strong> button`
-                    );
-                  }
-                )(),
-            },
+            this.createButton(config, "Cancel"),
+            this.createButton(config, "Save"),
           ],
         },
       ];
+    },
+
+    createFields(config) {
+      return config.fields.map((element) => ({
+        view: "text",
+        label: [element],
+        name: [element],
+      }));
+    },
+
+    createButton(config, type) {
+      return {
+        view: "button",
+        value: type,
+        css: type == "Save" ? "webix_primary" : "",
+        click: () =>
+          (
+            config[type.toLowerCase() + "Action"] ??
+            function () {
+              webix.message(
+                `No action was attached to the <strong>${type}</strong> button`
+              );
+            }
+          )(),
+      };
     },
   },
   webix.ui.form
